9bdf0fc3494c064ae16e877ed62696cf34be7894
leey0818 <leey0818@gmail.com>
2019-10-16 22:22:20 +0900

    lint 규칙 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     '@vue/airbnb',
   ],
   rules: {
+    'import/no-extraneous-dependencies': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },

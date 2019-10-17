358d6240e538f1667ecb2f9d1f8b9d038d057884
leey0818 <leey0818@gmail.com>
2019-10-17 23:24:40 +0900

    Friends, Rooms 탭 추가

--- src/components/ChatSidebar.vue
+++ src/components/ChatSidebar.vue
@@ -10,7 +10,7 @@
     dense
   >
     <v-list-item class="mb-2">
-      <v-list-item-avatar class="mr-3" color="grey" size="38">
+      <v-list-item-avatar class="mr-4" color="grey" size="38">
         <i class="fa fa-user-alt"></i>
       </v-list-item-avatar>
       <v-list-item-content>
@@ -22,32 +22,56 @@
       </v-list-item-content>
     </v-list-item>
 
-    <v-list-item>
-      <v-text-field
-        label="Friends"
-        tile
-        dense
-        full-width
-        hide-details
-      ></v-text-field>
-    </v-list-item>
-
-    <v-divider></v-divider>
-    <v-list-item
-      v-for="user in users"
-      :key="user.id"
-      link
+    <v-tabs
+      v-model="tab"
+      background-color="#434753"
+      dark
+      grow
     >
-      <v-list-item-avatar class="mr-3" color="grey" size="38">
-        <i class="fa fa-user-alt"></i>
-      </v-list-item-avatar>
-      <v-list-item-content>
-        <v-list-item-title class="pt-1 subtitle-1">{{ user.name }}</v-list-item-title>
-        <v-list-item-subtitle
-          :class="`status s-${user.state}`"
-        >{{ user.state }}</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
+      <v-tab :ripple="false">Friends</v-tab>
+      <v-tab :ripple="false">Rooms</v-tab>
+
+      <v-tab-item :transition="false" :reverse-transition="false">
+        <v-list class="br0" color="#434753" dark>
+          <v-list-item>
+            <v-text-field
+              label="Search"
+              tile
+              dense
+              full-width
+              hide-details
+            ></v-text-field>
+          </v-list-item>
+
+          <v-divider></v-divider>
+          <v-list-item
+            v-for="user in users"
+            :key="user.id"
+            link
+          >
+            <v-list-item-avatar class="mr-3" color="grey" size="38">
+              <i class="fa fa-user-alt"></i>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title class="pt-1 subtitle-1">{{ user.name }}</v-list-item-title>
+              <v-list-item-subtitle
+                :class="`status s-${user.state}`"
+              >{{ user.state }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-tab-item>
+      <v-tab-item :transition="false" :reverse-transition="false">
+        <v-list class="br0" color="#434753" dark>
+          <v-list-item link>
+            <v-list-item-content>
+              <v-list-item-title>첫번째 방</v-list-item-title>
+              <v-list-item-subtitle>nickname 외 2명</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-tab-item>
+    </v-tabs>
   </v-list>
 </template>
 
@@ -57,6 +81,7 @@ import { mapState } from 'vuex';
 export default {
   data() {
     return {
+      tab: 0,
       users: [
         { id: 0, name: 'leey0818', state: 'online' },
         { id: 1, name: 'Beans', state: 'offline' },
